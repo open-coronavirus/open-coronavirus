9a89115871b150245065ae19ff45049db8c9ec5a
Alberto Ordas <berto@nitsnets.com>
2020-04-05 20:20:43 +0200

    Fix welcome

--- app-citizen/src/app/welcome/welcome.component.ts
+++ app-citizen/src/app/welcome/welcome.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { NativeStorage } from '@ionic-native/native-storage/ngx';
 import { PatientService } from '../shared/services/patient.service';
+import { IonSlides } from '@ionic/angular';
 
 
 @Component({
@@ -12,6 +13,11 @@ import { PatientService } from '../shared/services/patient.service';
 })
 export class WelcomeComponent implements OnInit {
 
+    @ViewChild(IonSlides) slidesElement: IonSlides;
+
+    TOTAL_SLIDES = 3;
+
+    slideOpts: any;
     constructor(
         protected router: Router,
         protected nativeStorage: NativeStorage) {
@@ -19,11 +25,26 @@ export class WelcomeComponent implements OnInit {
 
     public ngOnInit(): void {
 
+        this.slideOpts = {
+            initialSlide: 0,
+            speed: 400
+        };
 
     }
 
-    public goToRegister() {
-        // this.router.navigate(['register']);
+    goContinue() {
+        // console.log("his.slidesElement.pager: ", this.slidesElement.pager);
+        // if (this.slidesElement.length < this.TOTAL_SLIDES) {
+        //     this.slidesElement.slideNext();
+        // } else {
+        //     this.goToSplash();
+        // }
+        this.goContinue();
+
+    }
+
+    public goToSplash() {
+        this.router.navigate(['splash']);
     }
 
 }
