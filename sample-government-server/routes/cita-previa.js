64f81cf3bcbc698c3827b39a926e73d28641da5e
Alejandro Such <alejandro.such@gmail.com>
2020-04-05 17:32:21 +0200

    Sample government server for demo purpouses

---
+++ sample-government-server/routes/cita-previa.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var moment = require('moment');
+var uuidv4 = require('uuid').v4;
+
+var router = express.Router();
+
+router.post('/', (_, res) => {
+  res.json({
+    codigoCita: uuidv4(),
+    centro: {
+      codigo: 'ALC:CESP:BBL',
+      nombre: 'Centro de especialidades de Babel',
+      coordenadas: {
+        latitud: 38.3546956,
+        longitud: -0.4983639,
+      },
+    },
+    fecha: moment().add('1 days').format('DD/MM/YYYY'),
+    hora: moment().format('HH:mm'),
+  });
+});
+
+module.exports = router;
