d9987fa9a7974486ae73261ff338aed9f9a01a4f
Alberto Ordas <berto@nitsnets.com>
2020-04-13 21:20:16 +0200

    Fix bug test

--- app-citizen/src/app/main/autotest/autotest.component.ts
+++ app-citizen/src/app/main/autotest/autotest.component.ts
@@ -129,10 +129,10 @@ export class AutotestComponent implements OnInit, OnDestroy {
                                 this.router.navigate(['/app/test-result/result/3']);
                             } else if (score >= 60) {
                                 this.router.navigate(['/app/test-result/result/4']);
-                            } else if (score === 10 || score === 20) {
+                            } else if (score === 10) {
                                 this.router.navigate(['/app/test-result/result/5']);
                             } else {
-                                // this.router.navigate(['/app/test-result/result/6']);
+                                this.router.navigate(['/app/test-result/result/5']);
                             }
                             break;
                         case TestType.FOLLING_UP:
